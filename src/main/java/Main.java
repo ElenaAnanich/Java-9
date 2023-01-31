6f51de330284f05898b1f41fa2d033d8c92f52f9
Lena k <ktyecz09@mail.ru>
2023-01-31 07:49:48 +0300

    add initial comit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Ivan";
+        post.passport = "11 11 № 111 111";
+        post.patronymic = "Ivanovich";
+        post.phone = "phone";
+        post.surname = "Ivanov";
+        post.subscription = "true";
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
+}

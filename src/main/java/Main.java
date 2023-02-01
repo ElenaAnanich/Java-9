e2801847261d767841f5cb027ad62c5f7a4d0bf8
Lena k <ktyecz09@mail.ru>
2023-02-01 06:40:27 +0300

    add initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
         post.patronymic = "Ivanovich";
         post.phone = "phone";
         post.surname = "Ivanov";
-        post.subscription = "true";
+        post.subscription = Boolean.parseBoolean("true");
         post.birthday = new FormDate();
         post.birthday.day = 13;
         post.birthday.month = 6;
